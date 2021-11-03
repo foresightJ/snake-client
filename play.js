0e5ddbfbac6cd25106e6cb1ebe7627c53750506c
John Foresight <foresight400@gmail.com>
2021-11-03 22:12:42 +0000

    bring on the progress

--- play.js
+++ play.js
@@ -2,28 +2,13 @@
 "use strict"
 
 const net = require("net");
+const {setupInput} = require("./input");
 const {connect} = require('./client')
 
 // establishes a connection with the game server
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function (key) {
-  // your code here
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
 
 console.log("Connecting ...");
 connect();
-handleUserInput();
 setupInput();
 
